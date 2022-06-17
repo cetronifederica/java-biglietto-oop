9ce72d935ecc30079653a1721eddcd83d8994456
federicacetroni <cetronifederica97@gmail.com>
2022-06-17 17:03:30 +0200

    aggiunto ciclo al bonus

--- src/biglietto/del/treno/BiglietteriaBonus.java
+++ src/biglietto/del/treno/BiglietteriaBonus.java
@@ -5,43 +5,44 @@ import java.util.Scanner;
 public class BiglietteriaBonus {
 
 	public static void main(String[] args) {
+
 		// devo stampare una scelta per l'utente : inserire nuovo biglietto o terminare
 		// il programma
 		// se scegli di inserire biglietto chiedo i dati
 		// altrimenti termino il programma
-
-		// Costruisco la struttura del menù
-
-		System.out.println("Fai la tua scelta tra queste due opzioni");
-		// 1
-		System.out.println("Per un Nuovo Biglietto digita 1");
-		// 0
-		System.out.println("Per chiudere il programma digita 0");
-
 		Scanner scan = new Scanner(System.in);
 
-		// prendo il valore
+		// Costruisco la struttura del menù
+		int r;
 
-		int r = scan.nextInt();
+		do {
+			System.out.println("Fai la tua scelta tra queste due opzioni");
+			// 1
+			System.out.println("Per un Nuovo Biglietto digita 1");
+			// 0
+			System.out.println("Per chiudere il programma digita 0");
+			r = scan.nextInt();
 
-		// Effettuo la verifica per l'evento selezionato
+			// Effettuo la verifica per l'evento selezionato
 
-		if (r == 0) {
+			if (r == 0) {
 
-			System.out.println("Programma terminato");
-		} else if (r == 1) {
+				System.out.println("Programma terminato");
+			} else if (r == 1) {
 
-			System.out.println("Benvenuto");
-			System.out.println("Inserisci la tua età: ");
-			int etaPasseggero = scan.nextInt();
-			System.out.println("Inserisci i chilometri che vuoi percorrere: ");
-			double chilometriDaPerc = scan.nextDouble();
+				System.out.println("Benvenuto");
+				System.out.println("Inserisci la tua età: ");
+				int etaPasseggero = scan.nextInt();
+				System.out.println("Inserisci i chilometri che vuoi percorrere: ");
 
-			Biglietto biglietto = new Biglietto(chilometriDaPerc, etaPasseggero);
-			// stampa del prezzo del biglietto
-			System.out.println("Il tuo biglietto costa: " + biglietto.prezzoBigliettoFormattato());
+				double chilometriDaPerc = scan.nextDouble();
 
-		}
+				Biglietto biglietto = new Biglietto(chilometriDaPerc, etaPasseggero);
+				// stampa del prezzo del biglietto
+				System.out.println("Il tuo biglietto costa: " + biglietto.prezzoBigliettoFormattato());
+
+			}
+		} while (r == 1);
 
 		scan.close();
 	}
